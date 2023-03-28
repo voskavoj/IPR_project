e7583df162764979d0a27ed7912a419c205c241a
Voska Vojtech <vojtech.voska@comap-control.com>
2023-03-28 18:54:09 +0300

    Code style refactor

--- stations/station_list.js
+++ stations/station_list.js
@@ -1,12 +1,15 @@
+// DOCUMENT
 const tabStationTable = document.querySelector('#station-table');
 
+
+// GLOBALS
 const fake_station_db = [
     ["Able", "Achaios", 1.997, 1.654, 6, 22],
     ["Baker", "Achaios", 1.998, 1.634, 15, 22],
     ["Charlie", "Pelopones", 1.954, 1.665, 0, 24],
     ["Dog", "Pelopones", 1.936, 1.646, 0, 24],
     ["Easy", "Pelopones", 1.868, 1.634, 0, 24],
-    ["Fox", "Korinth", 1.900, 1.643, 13, 24],
+    ["Fox", "Korinth", 1.900, 1.643, 23, 24],
     ["George", "Korinth", 1.902, 1.655, 3, 23],
     ["How", "Korinth", 1.990, 1.694, 13, 22],
     ["Item", "Patras", 1.944, 1.599, 12, 22],
@@ -15,22 +18,11 @@ const fake_station_db = [
 
 let stations = [];
 
-// NOTE!!!!!!!! ` not ' or "
 
-function fake_read_db()
-{
-    for (let i = 0; i < fake_station_db.length; i++)
-    {
-        let db_entry = fake_station_db[i];
-        let station = new Station(db_entry[0],
-            db_entry[1],
-            {"n95": db_entry[2], "diesel": db_entry[3]},
-            {"open": db_entry[4], "close": db_entry[5]});
-        stations.push(station);
-    }
-}
+// EVENT LISTENERS
 
 
+// CLASSES
 class Station {
     constructor(name, region, prices, opening_hours)
     {
@@ -79,14 +71,13 @@ class Station {
     {
         let current_time_min = new Date();
         current_time_min = current_time_min.getHours() * 60 + current_time_min.getMinutes();
-        console.log(current_time_min);
 
         if (this.is_nonstop)
             return true;
         else
-           return this.opening_hours.open * 60 < current_time_min && current_time_min < this.opening_hours.close * 60;
-            // return (Station._hh_mm_to_minutes(this.opening_hours.open) < current_time_min &&
-            //     Station._minutes_to_hh_mm(this.opening_hours.close) > current_time_min);
+            return this.opening_hours.open * 60 < current_time_min && current_time_min < this.opening_hours.close * 60;
+        // return (Station._hh_mm_to_minutes(this.opening_hours.open) < current_time_min &&
+        //     Station._minutes_to_hh_mm(this.opening_hours.close) > current_time_min);
     }
 
     _print_opening_hours()
@@ -118,7 +109,24 @@ function construct_table()
 
 }
 
+// FUNCTIONS
+function fake_read_db()
+{
+    for (let i = 0; i < fake_station_db.length; i++)
+    {
+        let db_entry = fake_station_db[i];
+        let station = new Station(db_entry[0],
+            db_entry[1],
+            {"n95": db_entry[2], "diesel": db_entry[3]},
+            {"open": db_entry[4], "close": db_entry[5]});
+        stations.push(station);
+    }
+}
 
-// main
-fake_read_db();
-construct_table();
+// PAGE FUNCTIONS
+function on_page_load()
+{
+    fake_read_db();
+    construct_table();
+}
+on_page_load(); // init
