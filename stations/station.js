1a4879deeb2ffe59cbcb53225b3fb1fc1c6dba97
Voska Vojtech <vojtech.voska@comap-control.com>
2023-03-28 19:20:13 +0300

    Implement station display

--- stations/station.js
+++ stations/station.js
@@ -2,6 +2,16 @@
 let parUrlParams = new URLSearchParams(document.location.search);
 let parStationId = parUrlParams.get("station");
 
+// fields to fill
+let fldName = document.querySelector("#station-name");
+let fldImage = document.querySelector("#station-image");
+let fldLocation = document.querySelector("#station-location");
+let fldPrices = document.querySelector("#station-prices");
+    let fldPricesN95 = document.querySelector("#station-prices-n95");
+    let fldPricesDiesel = document.querySelector("#station-prices-diesel");
+let fldOpHours = document.querySelector("#station-opening-hours");
+
+
 // GLOBALS
 const fake_station_db = [
     ["Able", "Achaios", 1.997, 1.654, 6, 22],
@@ -67,7 +77,6 @@ class Station {
     {
         let current_time_min = new Date();
         current_time_min = current_time_min.getHours() * 60 + current_time_min.getMinutes();
-        console.log(current_time_min);
 
         if (this.is_nonstop)
             return true;
@@ -97,12 +106,22 @@ class Station {
     }
 }
 
-function construct_table()
-{
-    for (let i = 0; i < stations.length; i++)
+class DisplayStation {
+    constructor(station)
     {
-        tabStationTable.appendChild(stations[i].to_html_table_row())
+        this.station = station;
     }
+
+    display()
+    {
+        document.title += " " + this.station.name;
+        fldName.innerHTML = this.station.name;
+        fldPricesN95.innerHTML = this.station.prices.n95;
+        fldPricesDiesel.innerHTML = this.station.prices.diesel;
+        fldLocation.innerHTML = this.station.region;
+        fldOpHours.innerHTML = this.station._print_opening_hours();
+    }
+
 
 }
 
@@ -138,8 +157,9 @@ function render_station(station_id)
         display_404();
         return;
     }
-    console.debug(station.name, station.region);
 
+    station = new DisplayStation(station);
+    station.display();
 
 }
 
@@ -150,6 +170,7 @@ function display_404()
     console.log(`No station found or no station ID given! Redirecting to 404.`);
     window.location.href = "station_404.html";
 }
+
 function on_page_load()
 {
     if (parStationId === null)
