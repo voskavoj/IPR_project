ff99df55544ef454d32bba00dda2665b1e12d5c7
Voska Vojtech <vojtech.voska@comap-control.com>
2023-05-16 22:23:39 +0300

    Implement Station_list as a template

--- server/station.mjs
+++ server/station.mjs
@@ -1,3 +1,5 @@
+import {Station} from "./station_list.mjs";
+
 export function route_station(req, res)
 {
     let station; // todo
@@ -9,4 +11,6 @@ function poll_station(station_name)
 {
     // todo DB
     // return one station
+    
+    return new Station(...["Dog", "Pelopones", 1.936, 1.646, 0, 24, 14.556464, 52.665445, "Georgios Mouranos", "able@fakeoil.gr", "+39445675435", "Ametitia 13\nPatra\n25664"]);
 }
